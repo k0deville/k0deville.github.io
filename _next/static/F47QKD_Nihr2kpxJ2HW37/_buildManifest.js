b9b6f28852172cbf15ef653df8cdc03ba34aba58
nihaldivyam <nihaldivyam@gmail.com>
2023-11-12 16:33:41 +0530

    Add initial website

---
+++ _next/static/F47QKD_Nihr2kpxJ2HW37/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/516-ac81501f1c662c3d.js","static/chunks/pages/index-c7da88414c8a083b.js"],"/_error":["static/chunks/pages/_error-4afcb85b7c260fd3.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
